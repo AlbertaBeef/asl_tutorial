423effca5d85423eb20b2ad3e53e5b68470efdb6
Mario Bergeron <grouby177@gmail.com>
2023-04-18 09:10:06 -0400

    Update ASL notebook.

--- prepare_docker.sh
+++ prepare_docker.sh
@@ -1,9 +1,20 @@
 #/bin/bash
 
-#docker pull xilinx/vitis-ai-cpu:2.5
+# req:
+#   docker pull xilinx/vitis-ai-pytorch-cpu
+#   docker pull xilinx/vitis-ai-tensorflow2-cpu
+# usage:
+#   ./docker_run.sh xilinx/vitis-ai-pytorch-cpu
+#   ./docker_run.sh xilinx/vitis-ai-tensorflow2-cpu
 
-wget https://github.com/Xilinx/Vitis-AI/raw/v2.5/docker_run.sh
-wget https://github.com/Xilinx/Vitis-AI/raw/v2.5/docker/dockerfiles/PROMPT.txt
-
-sed -i 's/.\/docker\/dockerfiles\/PROMPT.txt/PROMPT.txt/g' docker_run.sh
+wget https://github.com/Xilinx/Vitis-AI/raw/v3.0/docker_run.sh
 chmod a+x docker_run.sh
+
+wget https://github.com/Xilinx/Vitis-AI/raw/v3.0/docker/dockerfiles/PROMPT/PROMPT_cpu.txt
+wget https://github.com/Xilinx/Vitis-AI/raw/v3.0/docker/dockerfiles/PROMPT/PROMPT_gpu.txt
+wget https://github.com/Xilinx/Vitis-AI/raw/v3.0/docker/dockerfiles/PROMPT/PROMPT_rocm.txt
+
+mkdir -p docker/dockerfiles/PROMPT
+mv PROMPT_cpu.txt docker/dockerfiles/PROMPT/.
+mv PROMPT_gpu.txt docker/dockerfiles/PROMPT/.
+mv PROMPT_rocm.txt docker/dockerfiles/PROMPT/.
